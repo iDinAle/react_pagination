93dba445a3f9c1fbb11bf525fb253503cceb8137
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-16 18:25:27 +0200

    Updates

---
+++ precache-manifest.b78f7bd456d467e5f569b7b8c04efb7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fad9a9ddbc5c93f322755e00951fcc2",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "630e7f6c47e7cc9ec816",
+    "url": "/react_pagination/static/css/main.b6401dd8.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react_pagination/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "630e7f6c47e7cc9ec816",
+    "url": "/react_pagination/static/js/main.c493a153.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
